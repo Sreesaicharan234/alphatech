3c7d4d70c4b3d8da80ce707ea75aeed86fa06694
sathish <sathish.gollapally@gmail.com>
2023-07-24 20:28:56 +0530

    removed unused imports.

--- src/main/java/com/alphatech/ems/employee/management/controller/HomeController.java
+++ src/main/java/com/alphatech/ems/employee/management/controller/HomeController.java
@@ -2,9 +2,7 @@ package com.alphatech.ems.employee.management.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.logging.Logger;
 
 @CrossOrigin("*")
 @RestController
@@ -12,11 +10,9 @@ import java.util.logging.Logger;
 @Slf4j
 public class HomeController {
 
-
-
     @GetMapping
     public String getCurrentTime() {
-        log.info("Service: Fetching user with id {}"+ LocalDateTime.now());
+        log.info("Service: Fetching user with id {} ", LocalDateTime.now());
         return "Hi, Time now: " + LocalDateTime.now();
     }
 
